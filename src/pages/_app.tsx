7fe3665613287c907d8680f49e6b5192bd01eeb3
rafaelst2000 <rafaelst2000@gmail.com>
2023-04-11 10:01:12 -0300

    feat: home page done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
-import logoImg from '@/assets/logo.svg'
+import { globalStyles } from '@/styles/global'
 import { Container, Header } from '@/styles/pages/app'
+import Image from 'next/image'
+
+import logoImg from '@/assets/logo.svg'
 
 globalStyles()
 
@@ -10,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>

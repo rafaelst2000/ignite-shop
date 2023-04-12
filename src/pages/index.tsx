585a8a9718e364e3114549b779a946ebfc9cb192
rafaelst2000 <rafaelst2000@gmail.com>
2023-04-12 08:51:21 -0300

    feat: next Link component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { HomeContainer, Product } from "@/styles/pages/home"
 import { useKeenSlider } from 'keen-slider/react'
 import { stripe } from "@/lib/stripe"
 import { GetStaticProps } from "next"
+import Link from 'next/link'
 import Stripe from "stripe"
 import Image from "next/image"
 import 'keen-slider/keen-slider.min.css'
@@ -27,14 +28,16 @@ export default function Home({ products }: HomeProps) {
    <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
    </HomeContainer>

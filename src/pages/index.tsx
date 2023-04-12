271d2a73ea1306e935d4fa90fe934b2e29c558d0
rafaelst2000 <rafaelst2000@gmail.com>
2023-04-12 10:49:40 -0300

    feat: finish app without challenge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { GetStaticProps } from "next"
 import Link from 'next/link'
 import Stripe from "stripe"
 import Image from "next/image"
+import Head from 'next/head'
 import 'keen-slider/keen-slider.min.css'
 
 interface HomeProps {
@@ -12,7 +13,7 @@ interface HomeProps {
     id: string
     name: string
     imageUrl: string
-    price: number
+    price: string
   }[]
 }
 
@@ -25,22 +26,27 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-   <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map((product) => {
-        return (
-          <Link href={`/product/${product.id}`} key={product.id}>
-            <Product className="keen-slider__slide">
-              <Image src={product.imageUrl} width={520} height={480} alt="" />
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+          {products.map((product) => {
+            return (
+              <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+                <Product className="keen-slider__slide">
+                  <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
-          </Link>
-        )
-      })}
-   </HomeContainer>
+                  <footer>
+                    <strong>{product.name}</strong>
+                    <span>{product.price}</span>
+                  </footer>
+                </Product>
+              </Link>
+            )
+          })}
+      </HomeContainer>
+    </>
   )
 }
 

70a9fbf51c137dedce1a304317337e024b4fe960
rafaelst2000 <rafaelst2000@gmail.com>
2023-04-14 14:47:45 -0300

    fix: useBreakpoint hook now starts with right value

--- src/hooks/useBreakpoint.ts
+++ src/hooks/useBreakpoint.ts
@@ -1,16 +1,22 @@
-import { useLayoutEffect, useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 export function useBreakpoint() {
-  const [breakpoint, setBreakpoint] = useState('mobile')
+  const [windowWidth, setWindowWidth] = useState(0)
+  const breakpoint = windowWidth < 768 ? 'mobile' : 'desktop'
+  const initialViewport = useRef(
+    typeof window !== 'undefined' ? window.innerWidth : 0,
+  )
 
-  useLayoutEffect(() => {
-    const mobileQuery = window.matchMedia('(max-width: 768px)')
-    const handleBreakpointChange = (event: MediaQueryListEvent) => {
-      setBreakpoint(event.matches ? 'mobile' : 'desktop')
-    }
-    mobileQuery.addEventListener('change', handleBreakpointChange)
-    return () => {
-      mobileQuery.removeEventListener('change', handleBreakpointChange)
+  function handleResize() {
+    setWindowWidth(window.innerWidth)
+  }
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', handleResize)
+      setWindowWidth(initialViewport.current)
+      return () => {
+        window.removeEventListener('resize', handleResize)
+      }
     }
   }, [])
 
